1d1bd9f13d80f0648bd841559a39f55c6d7fe14e
Musa Sayar <musasayar67@gmail.com>
2024-12-07 13:12:27 +0300

    added env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {FIREBASE_API_KEY, FIREBASE_APP_ID} from "@env"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMvGLaczyreIvPBO8TeWCzpg82t3Hb3VY",
+  apiKey: FIREBASE_API_KEY,
   authDomain: "social-app-25035.firebaseapp.com",
   projectId: "social-app-25035",
   storageBucket: "social-app-25035.firebasestorage.app",
   messagingSenderId: "1064215810856",
-  appId: "1:1064215810856:web:9ad6feb6550f94768df61d",
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
